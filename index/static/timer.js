2b7dd3aa34203c9fed5ec28cf94b6612182bd7f9
MacroXie <xiehongzhe04@gmail.com>
2024-10-08 22:45:06 -0700

    Fixed an issue where timers can only be used on the first element

--- index/static/timer.js
+++ index/static/timer.js
@@ -1,39 +1,44 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var timberElement = document.getElementById('timer');
-    if (!timberElement) return;
+    // 使用 querySelectorAll 选取所有具有 'timer' 类和 'timer' ID 的元素
+    var timerElements = document.querySelectorAll('.timer[id="timer"]');
 
-    // 获取日期字符串，优先从 data-time 属性中获取
-    var dateString = timberElement.getAttribute('data-time');
-    if (!dateString) {
-        dateString = timberElement.innerText;
-    }
-
-    // 将日期字符串解析为 Date 对象
-    var countDownDate = new Date(dateString);
-    if (isNaN(countDownDate)) {
-        // 日期解析失败，显示错误信息
-        timberElement.innerText = "No Due Date";
-        return;
-    }
-
-    // 开始倒计时
-    var x = setInterval(function() {
-        var now = new Date().getTime();
-        var distance = countDownDate - now;
-
-        if (distance <= 0) {
-            clearInterval(x);
-            timberElement.innerText = "Due Date has passed";
+    // 遍历每个计时器元素
+    timerElements.forEach(function(timberElement) {
+        // 获取日期字符串，优先从 data-time 属性中获取
+        var dateString = timberElement.getAttribute('data-time');
+        if (!dateString) {
+            console.error('No data-time attribute found for element', timberElement);
+            timberElement.innerText = "No Due Date";
             return;
         }
 
-        // 计算剩余的天、小时、分钟、秒
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 *60 *24)) / (1000 *60 *60));
-        var minutes = Math.floor((distance % (1000 *60 *60)) / (1000 *60));
-        var seconds = Math.floor((distance % (1000 *60)) / 1000);
+        // 将日期字符串解析为 Date 对象
+        var countDownDate = new Date(dateString);
+        if (isNaN(countDownDate.getTime())) {
+            timberElement.innerText = "Invalid Due Date";
+            return;
+        }
 
-        // 更新页面显示
-        timberElement.innerText = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds";
-    }, 1000);
+        // 开始倒计时
+        var x = setInterval(function() {
+            var now = new Date().getTime();
+            var distance = countDownDate - now;
+
+            if (distance < 0) {
+                clearInterval(x);
+                timberElement.innerText = "Due Date has passed";
+                return;
+            }
+
+            // 计算剩余的天、小时、分钟、秒
+            var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+            var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+            var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+            // 更新页面显示
+            timberElement.innerText = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds";
+
+        }, 1000);
+    });
 });
